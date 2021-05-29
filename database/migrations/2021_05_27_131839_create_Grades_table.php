6d3ace77d7ac99f196a3f2b01a7797e4a6f9e283
MahmoudShalma <mahmoud.shalma@outlook.com>
2021-05-29 03:20:45 +0200

    AddUpdatAndDeleteGrades

---
+++ database/migrations/2021_05_27_131839_create_Grades_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateGradesTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('Grades', function(Blueprint $table) {
+			$table->increments('id');
+            $table->string('Name')->unique();
+            $table->string('Notes')->nullable();
+			$table->timestamps();
+
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('Grades');
+	}
+}
